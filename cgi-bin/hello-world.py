02b9c14d5f7648be11236254391b4596664daee1
Austine D. Odhiambo <austine.declan@gmail.com>
2022-04-22 23:42:20 +0300

    Postgress stuff

--- cgi-bin/hello-world.py
+++ cgi-bin/hello-world.py
@@ -3,6 +3,11 @@ print("Status: 302")
 print("Location: ./index.html?msg=executed")
 print('Content-Type: text/html\n')
 
+DB_HOST = ''
+DB_NAME = ''
+DB_USER = ''
+DB_PASS = ''
+
 import cgi
 
 form = cgi.FieldStorage()
