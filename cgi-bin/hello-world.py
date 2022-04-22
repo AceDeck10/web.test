0bb32c365ff9a529949d2fbd5681ddc3cadc5ba7
Austine D. Odhiambo <austine.declan@gmail.com>
2022-04-22 14:31:00 +0300

    Random experiments

--- cgi-bin/hello-world.py
+++ cgi-bin/hello-world.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+print("Status: 302")
+print("Location: ./index.html?msg=executed")
 print('Content-Type: text/html\n')
 
 import cgi
@@ -9,5 +10,6 @@ form = cgi.FieldStorage()
 name = form.getvalue('name')
 passwd = form.getvalue('password')
 
+print()
 print(name)
 print(passwd)
