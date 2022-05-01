69124a5ea642cae0e0d1f615035355b08f32b70e
Austine D. Odhiambo <austine.declan@gmail.com>
2022-05-01 23:52:29 +0300

    Incomplete urllib implementation but I dont want to lose what Ive done so far

--- cgi-bin/hello-world.py
+++ cgi-bin/hello-world.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-print("Status: 302")
-print("Location: ./index.html?msg=executed")
-print('Content-Type: text/html\n')
 
 DB_HOST = 'localhost'
 DB_NAME = 'webtest_db'
@@ -9,8 +6,20 @@ DB_USER = 'postgres'
 DB_PASS = '@c3c0m10'
 
 import cgi
+import http
+from urllib.parse import urlparse
 import psycopg2
 import requests
+import urllib
+
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        name = 'World'
+        query_components = parse_qs(urlparse(self.path).query)
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
