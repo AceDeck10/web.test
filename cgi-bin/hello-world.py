f9d6b305a9f8ea788506c2df61b49b9a4dcea461
Austine D. Odhiambo <austine.declan@gmail.com>
2022-05-07 13:14:34 +0300

    cgi sucks. A simple problem that should have a simple solution has me wanting to die

--- cgi-bin/hello-world.py
+++ cgi-bin/hello-world.py
@@ -6,14 +6,8 @@ DB_USER = 'postgres'
 DB_PASS = '@c3c0m10'
 
 import cgi
-import http
-import http.server
-import socketserver
-from urllib import request
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
 import psycopg2
-import requests
+
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
@@ -32,30 +26,7 @@ cur.execute('SELECT * FROM tbl_users')
 allUsers = cur.fetchall()
 cur.close()
 
-#    print('Id = ', row[0])
-#    print('Username = ', row[1])
-#    print('Password = ', row[2])
-#    print('<br/>')
-
 print("Status: 303\r\n")
 print("Location: http://127.0.0.1:8000/index.html/\r\n")
 print("Content-Type: text/html\r\n")
 #print("Location: http://127.0.0.1:8000/index.html?name=" + name + "&Password=" + passwd"/r/n")
-
-#class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-#    def do_GET(self):
-#       self.send_response(302)
-#       self.send_header("Content-type", "text/html")
-#       self.end_headers()
-#       self.path = "http://127.0.0.1:8000/index.html"
-#
-#       return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-#handler_object = HTTPRequestHandler
-
-#PORT = 8000
-#my_server = socketserver.TCPServer(("", PORT), handler_object)
-
-#my_server.serve_forever()
-
-
